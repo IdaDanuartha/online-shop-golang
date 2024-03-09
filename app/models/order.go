98b4320987f447915b71df8296df062c75d236d2
Ida Danuartha <danuart21@gmail.com>
2024-03-09 20:45:24 +0800

    fix typo

--- app/models/order.go
+++ app/models/order.go
@@ -9,16 +9,16 @@ import (
 
 type Order struct {
 	ID                  string `gorm:"size:36;not null;uniqueIndex;primary_key"`
-	User                []User
+	User                User
 	UserID              string `gorm:"size:36;index"`
 	OrderItems          []OrderItem
-	OrderCustomer       *OrderCustomer
+	OrderCustomer       *OrderCustomer // has one
 	Code                string `gorm:"size:50;index"`
 	Status              int
 	OrderDate           time.Time
 	PaymentDue          time.Time
-	PaymentStatus       string          `gorm:"size:50;index`
-	PaymentToken        string          `gorm:"size:100;index"`
+	PaymentStatus       string          `gorm:"size:50;index"`
+	PaymentToken        string          `gorm:"size:100;index""`
 	BaseTotalPrice      decimal.Decimal `gorm:"type:decimal(16,2)"`
 	TaxAmount           decimal.Decimal `gorm:"type:decimal(16,2)"`
 	TaxPercent          decimal.Decimal `gorm:"type:decimal(10,2)"`
@@ -26,12 +26,12 @@ type Order struct {
 	DiscountPercent     decimal.Decimal `gorm:"type:decimal(10,2)"`
 	ShippingCost        decimal.Decimal `gorm:"type:decimal(16,2)"`
 	GrandTotal          decimal.Decimal `gorm:"type:decimal(16,2)"`
-	Note                string          `gorm:"type:text`
-	ShippingCourier     string          `gorm:"size:100`
-	ShippingServiceName string          `gorm:"size:100`
-	ApprovedBy          string          `gorm:"size:36`
+	Note                string          `gorm:"type:text"`
+	ShippingCourier     string          `gorm:"size:100"`
+	ShippingServiceName string          `gorm:"size:100"`
+	ApprovedBy          string          `gorm:"size:36"`
 	ApprovedAt          time.Time
-	CancelledBy         string `gorm:"size:36;index`
+	CancelledBy         string `gorm:"size:36;index"`
 	CancelledAt         time.Time
 	CancellationNote    string `gorm:"size:255"`
 	CreatedAt           time.Time
--- app/models/order_item.go
+++ app/models/order_item.go
@@ -8,7 +8,7 @@ import (
 
 type OrderItem struct {
 	ID              string `gorm:"size:36;not null;uniqueIndex;primary_key"`
-	Order           []Order
+	Order           Order
 	OrderID         string `gorm:"size:36;index"`
 	Product         []Product
 	ProductID       string `gorm:"size:36;index"`
@@ -20,8 +20,8 @@ type OrderItem struct {
 	DiscountAmount  decimal.Decimal `gorm:"type:decimal(16,2)"`
 	DiscountPercent decimal.Decimal `gorm:"type:decimal(10,2)"`
 	SubTotal        decimal.Decimal `gorm:"type:decimal(16,2)"`
-	Sku             string          `gorm:"size:36;index`
-	Name            string          `gorm:"size:255`
+	Sku             string          `gorm:"size:36;index"`
+	Name            string          `gorm:"size:255"`
 	Weight          decimal.Decimal `gorm:"type:decimal(10,2)"`
 	CreatedAt       time.Time
 	UpdatedAt       time.Time
